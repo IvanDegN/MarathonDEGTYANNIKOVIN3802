631fd001224b7dbca900c401e81b532c40ef7715
Дегтянников Иван Николаевич <10180030@college.local>
2021-04-02 13:56:11 +0300

    Добавьте файлы проекта.

---
+++ Runner`sMenu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarathonDEGTYANNIKOVIN3802
+{
+    /// <summary>
+    /// Логика взаимодействия для Runner_sMenu.xaml
+    /// </summary>
+    public partial class Runner_sMenu : Window
+    {
+        public Runner_sMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
